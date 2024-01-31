e6b7fa4004c227ee9c83597c9b2ef3ffde80bc9d
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-01-31 17:13:32 +0300

    tesr

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let carusel2 = document.querySelector('.carusel2');
 let btn5 = document.querySelector('#btn5');
 let btn6 = document.querySelector('#btn6');
 let img2 = document.querySelectorAll('.carusel2 img');
-
+//------------------------------------------------------
 let sum = 0 ;
 
 // console.log(img);
@@ -80,4 +80,35 @@ function cekBtn6() {
         sum = img2.length-1;
     }
     carusel2.style.transform = `translateX(${-sum * 1230}px)`
+}
+
+//----------------------------------------------------------
+
+let wrapper = document.querySelector('#wrapper');
+for(let i = 0 ; i < 10 ; i++){
+    let card = document.createElement('div');
+    card.innerHTML=`
+     <div class="w-[230px]  p-[10px] pl-[20px] bg-white rounded-[10px] relative">
+       <img src="./assets/imgs/image11.png" alt="img">
+       <p class="text-[#0D63F3] text-[18px] font-[591] mb-[6px]">
+           299 000 so’m
+       </p>
+       <p class="text-[#1E1E1E] text-[14px] font-[539] mb-[33px]">
+           Apple Airpods Pro <br>
+           simsiz quloqchin , Oq rangda
+       </p>
+       <p class="text-[14px] font-[539] text-[#6C757D] mb-[25px]">
+           1230 ta buyurtma
+        </p>
+        <span class="flex items-center gap-[36px]">
+        <button class="rounded-lg bg-[#0D63F3] py-[10px] px-[24px] hover:bg-blue-800">
+            <a href="#" class="text-white">Sotib olish</a>
+        </button>
+        <img src="./assets/imgs/shopping.svg" alt="shopping">
+        </span>
+        <img src="./assets/imgs/favorite.svg" alt="favorite" class="absolute top-3 right-3">
+      </div>
+    `;
+    wrapper.appendChild(card);
+
 }
