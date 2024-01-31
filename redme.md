98ed2f8a88ad34fa8fbc5a4f5466e669e4065d08
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-01-31 13:01:55 +0300

    homwork end

--- redme.md
+++ redme.md
@@ -152,3 +152,61 @@ element.style.color = "red";
        - **elament.classList.contans("")** - bu elament ichida biz kiritgan class bo'lsa true aks holda false qiymat qaytaradi.
 
         - **elament.classList.toggle("")** - bu biz kiritgan classimiz elamentlar classi ichida bo'lsa o'chiradi , agar yoq bo'lsa qo'shadi.
+
+<hr>        
+
+###  31. 01. 2024
+
+###  Dynamicolly DOM monupulating
+
+ - createElement(tegName);
+ - appennd();
+ - prepend();
+ - after();
+ - before();
+
+
+ 1. **createElement(tegName)** -> bizga yangi html elament qurib beradi .
+
+ ```
+ let div = document.createElement(div);
+
+ // natija -> <div></div>
+ ``` 
+
+ 2. **appennd()** -> yaratgan elamentimizni tanlab olgan elamentimiz ichiga joylashtirib beradi , joylashtirishda elamentimizni ohiridan joylashtiradi.
+
+ ```
+let wrapper =document.querySelector('.wrapper');
+let card = document.createElement('div');
+
+wrapper.append(card);
+ ```
+
+ 3. **prepend()** -> yaratgan elamentimizni tanlab olgan elamentimiz ichiga joylashtirib beradi , huddi 'appennd()' singari farqi bunda joylashtirishda elamentimiz boshidan  joylashtiradi, ikkisini bir vaqitda ishlatilmaydi .
+
+  ```
+ let wrapper =document.querySelector('.wrapper');
+  let card = document.createElement('div');
+
+  wrapper.prepend(card);
+ ```
+ 
+ 4. **after()** -> yaratgan elamentimizni tanlab olgan elamentimizni pasidan qo'shib beradi.
+
+ 
+  ```
+   let wrapper=document.querySelector('.wrapper');
+   let card = document.createElement('div');
+
+   wrapper.after(card);
+  ```
+
+5 . **before()** -> yaratgan elamentimizni tanlab olgan elamentimizni yuqorisidan qo'shib beradi.
+
+    ```
+    let wrapper =document.querySelector('.wrapper');
+    let card = document.createElement('div');
+
+    wrapper.before(card);
+    ```
--- script.js
+++ script.js
@@ -3,6 +3,16 @@ let carusel = document.querySelector('.carusel');
 let btn1 = document.querySelector('#btn1');
 let btn2 = document.querySelector('#btn2');
 let img = document.querySelectorAll('.carusel img');
+//----------------------------------------1 carusel
+let carusel1 = document.querySelector('.carusel1');
+let btn3 = document.querySelector('#btn3');
+let btn4 = document.querySelector('#btn4');
+let img1 = document.querySelectorAll('.carusel1 img');
+//----------------------------------------------------
+let carusel2 = document.querySelector('.carusel2');
+let btn5 = document.querySelector('#btn5');
+let btn6 = document.querySelector('#btn6');
+let img2 = document.querySelectorAll('.carusel2 img');
 
 let sum = 0 ;
 
@@ -26,4 +36,48 @@ function cekBtn2() {
         sum = img.length-1;
     }
     carusel.style.transform = `translateX(${-sum * 1230}px)`
+}
+
+//----------------------------------------------------------------------------
+
+function cekBtn3() {
+    sum++;
+    if(sum > img1.length-1){
+        sum = 0;
+    }else if(sum < 0){
+        sum = img1.length-1;
+    }
+    carusel1.style.transform = `translateX(${-sum * 1230}px)`
+}
+
+function cekBtn4() {
+    sum--;
+    if(sum > img1.length-1){
+        sum = 0;
+    }else if(sum < 0){
+        sum = img1.length-1;
+    }
+    carusel1.style.transform = `translateX(${-sum * 1230}px)`
+}
+
+//----------------------------------------------------------------------
+
+function cekBtn5() {
+    sum++;
+    if(sum > img2.length-1){
+        sum = 0;
+    }else if(sum < 0){
+        sum = img2.length-1;
+    }
+    carusel2.style.transform = `translateX(${-sum * 1230}px)`
+}
+
+function cekBtn6() {
+    sum--;
+    if(sum > img2.length-1){
+        sum = 0;
+    }else if(sum < 0){
+        sum = img2.length-1;
+    }
+    carusel2.style.transform = `translateX(${-sum * 1230}px)`
 }
